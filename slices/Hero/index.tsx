205a02c91a54b29043d9cb01320d6cba32f0ada1
Anatolii Fenin <fenintolja@gmail.com>
2026-04-15 14:14:06 +0300

    feat: implement Hero slice with 3D keyboard scene and keycap components

---
+++ slices/Hero/index.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { Bounded } from "@/components/Bounded";
+import { Content } from "@prismicio/client";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { Canvas } from "@react-three/fiber";
+import { FC } from "react";
+import Scene from "./Scene";
+
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+const Hero: FC<HeroProps> = ({ slice }) => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className="relative h-dvh text-white text-shadow-black/30 text-shadow-lg"
+    >
+      <div className="hero-scene blue-gradient-bg pointer-events-none sticky top-0 h-dvh w-full">
+        {/* Canvas go here */}
+        <Canvas shadows="soft">
+          <Scene />
+        </Canvas>
+      </div>
+
+      <div className="hero-content absolute inset-x-0 top-0 h-dvh">
+        <Bounded
+          fullWidth
+          className="absolute inset-x-0 top-18 md:top-24 md:left-[8vw]"
+        >
+          <PrismicRichText
+            field={slice.primary.heading}
+            components={{
+              heading1: ({ children }) => (
+                <h1 className="hero-heading font-black-slanted text-6xl leading-[0.8] uppercase sm:text-7xl lg:text-8xl">
+                  {children}
+                </h1>
+              ),
+            }}
+          />
+        </Bounded>
+
+        <Bounded
+          fullWidth
+          className="hero-body absolute inset-x-0 bottom-0 md:right-[8vw] md:left-auto"
+          innerClassName="flex flex-col gap-3"
+        >
+          <div className="max-w-md">
+            <PrismicRichText
+              field={slice.primary.body}
+              components={{
+                heading2: ({ children }) => (
+                  <h2 className="font-bold-slanted mb-1 text-4xl uppercase lg:mb-2 lg:text-6xl">
+                    {children}
+                  </h2>
+                ),
+              }}
+            />
+          </div>
+
+          <button className="font-bold-slanted group flex w-fit cursor-pointer items-center gap-1 rounded bg-[#01A7E1] px-3 py-1 text-2xl uppercase transition disabled:grayscale">
+            {slice.primary.buy_button_text}
+            <span className="transition group-hover:translate-x-1">{">"}</span>
+          </button>
+        </Bounded>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
